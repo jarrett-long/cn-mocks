fd616b16b787fdee6912c2057dff481c242f591e
Jarrett Long <JLong@rightpoint.com>
2021-09-13 18:50:03 -0500

    Init material ui

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,17 +2,22 @@ import React from "react";
 import Image from "next/image";
 import TpLink from "../TpLink/TpLink";
 import styles from "./header.module.css";
+import { Grid } from "@material-ui/core";
 
 function Header() {
   return (
     <header className={styles.header}>
-      <TpLink className="link" href="/">
-        <Image src="/logo.png" width={200} height={60} />
-      </TpLink>
-      <div className={styles.linksWrapper}>
-        <TpLink href="/organizations">Organizations</TpLink>
-        <TpLink href="/profile">My Profile</TpLink>
-      </div>
+      <Grid container justifyContent="space-between" alignItems="center">
+        <Grid item>
+          <TpLink href="/">
+            <Image src="/logo.png" width={200} height={60} />
+          </TpLink>
+        </Grid>
+        <Grid item xs={2}>
+          <TpLink href="/organizations">Organizations</TpLink>
+          <TpLink href="/profile">My Profile</TpLink>
+        </Grid>
+      </Grid>
     </header>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,36 @@
-import Layout from '../components/Layout/Layout'
-import '../styles/globals.css'
+import "@fontsource/roboto";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Head from "next/head";
+import Layout from "../components/Layout/Layout";
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#336699',
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: '#096',
+      contrastText: '#fff'
+    }
+  }
+});
 
 function App({ Component, pageProps }) {
-  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
-  return getLayout(<Component {...pageProps} />)
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <CssBaseline />
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
